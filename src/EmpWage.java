9c9cb906245732b36a3e33431271f7093847794c
rishika7-hub <srishika970@gmail.com>
2020-10-14 14:41:29 +0530

    Daily Wages

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,20 +1,33 @@
 import java.util.*;
 public class EmpWage {
+	
+	private static final int WAGE_PER_HOUR=20;
+	private static final int FULL_DAY_HOUR=8;
+	private static final int DAILY_WAGE=WAGE_PER_HOUR * FULL_DAY_HOUR;
 
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Problem");
-		// Employee Attendance
-		
-		if(isEmployeePresent()) {
-			System.out.println("Employee is Present");
-		} else {
-			System.out.println("Employee is Absent");
-	        }
-	    }
+				System.out.println("Welcome to Employee Wage Computation Problem");
+				
+				// Employee Attendance
+				
+				if(isEmployeePresent()) {
+					System.out.println("Employee is Present");
+				} else {
+					System.out.println("Employee is Absent");
+			        }
+	}
 
-	public static boolean isEmployeePresent() {
+	private static boolean isEmployeePresent() {
 		// TODO Auto-generated method stub
 		return new Random().nextBoolean();
 	}
 }
+	
+	
+
+
+	
+
+
