8dc6c6049c8c6fc7bad14cd9c66161c67a0186c6
rishika7-hub <srishika970@gmail.com>
2020-10-14 14:46:48 +0530

    Part time Employee Wages

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -4,6 +4,8 @@ public class EmpWage {
 	private static final int WAGE_PER_HOUR=20;
 	private static final int FULL_DAY_HOUR=8;
 	private static final int DAILY_WAGE=WAGE_PER_HOUR * FULL_DAY_HOUR;
+	private static final int PART_DAY_HOUR=4;
+	private static final int PART_TIME_WAGE=WAGE_PER_HOUR * PART_DAY_HOUR;
 
 	
 	public static void main(String[] args) {
