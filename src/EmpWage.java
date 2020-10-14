8727c0519d2952df67c31609527ff0c6bf49cc79
rishika7-hub <srishika970@gmail.com>
2020-10-14 17:09:52 +0530

    Updated Monthly wages

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -8,9 +8,8 @@ public class EmpWage {
 	private static final int PART_TIME_WAGE=WAGE_PER_HOUR * PART_DAY_HOUR;
 	public static final int IS_PART_TIME=2;
 	public static final int IS_FULL_TIME=1;
-	private static final int WORKING_DAYS_PER_MONTH=20;
+	private static final int WORKING_DAYS_PER_MONTH = 0;
 	
-
 
 	public static void main(String[] args) {
 				// TODO Auto-generated method stub
@@ -19,6 +18,7 @@ public class EmpWage {
 				int empHrs = 0;
 				int empWage=0;
 				int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
+				
 				switch (empCheck()) {
 				case IS_FULL_TIME: 
 					empHrs = 8;
@@ -28,9 +28,10 @@ public class EmpWage {
 					break;
 				default:
 					break;
-				} 
+				}
+				empWage= empHrs * WAGE_PER_HOUR;
+				System.out.println("Emp Wage:" +empWage);
 				
-				//Wages For Month
 					int wages = 0;
 					for (int i = 1; i <= WORKING_DAYS_PER_MONTH; i++) {
 						System.out.println("Day #" + i);
@@ -44,14 +45,7 @@ public class EmpWage {
 					return;
 				}
 				
-				
-				
-				
-				
-				
-		
-				
-			
+
 	private static int empCheck() {
 		// TODO Auto-generated method stub
 		return 0;
