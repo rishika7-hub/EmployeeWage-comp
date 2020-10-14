8e26d845cf7a1f864275ea7882e4dec6b136c900
rishika7-hub <srishika970@gmail.com>
2020-10-14 19:39:07 +0530

    conditions

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -8,8 +8,8 @@ public class EmpWage {
 	private static final int PART_TIME_WAGE=WAGE_PER_HOUR * PART_DAY_HOUR;
 	public static final int IS_PART_TIME=2;
 	public static final int IS_FULL_TIME=1;
-	public static final int NUM_OF_DAYS_IN_MONTH = 0;
-	
+	public static final int NUM_OF_DAYS_IN_MONTH = 20;
+	public static final int MAX_HRS_IN_MONTH =100;
 
 	public static void main(String[] args) {
 				// TODO Auto-generated method stub
@@ -20,34 +20,37 @@ public class EmpWage {
 					System.out.println("Employee is ABSENT");
 				}
 				
-				//  monthly wages
-				
-				int empHrs = 0;
-				int empWage=0;
-				int totalEmpWage = 0;
-				// int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
-				for (int day=0; day < NUM_OF_DAYS_IN_MONTH; day++) {
+		
+				 int empHrs = 0;
+					int empWage=0;
+					int totalWorkingDays = 0;
+	                int totalEmpHrs=0;
+	                while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_DAYS_IN_MONTH) {
+	                // int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
+					
 					int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
-				
-				switch (empCheck()) {
-				case IS_FULL_TIME: 
-					empHrs = 8;
-					break;
-				case IS_PART_TIME:
-					empHrs = 4;
-					break;
-				default:
-					empHrs=0;
-				}
-				empWage= empHrs * WAGE_PER_HOUR;
-				totalEmpWage += empWage;
-				System.out.println("Emp Wage : " + empWage);
-				 System.out.println("Total Employee Wage : " + totalEmpWage);
-				}
-				System.out.println("Total Employee Wage : " + totalEmpWage);
-    }
+					
+					switch (empCheck()) {
+					case IS_FULL_TIME: 
+						empHrs = 8;
+						break;
+					case IS_PART_TIME:
+						empHrs = 4;
+						break;
+					default:
+						empHrs=0;
+					}
+					totalEmpHrs+=empHrs;
+					empWage= empHrs * WAGE_PER_HOUR;
+					empWage += empWage;
+					System.out.println("Emp Wage : " + empWage);
+					System.out.println("Total Employee Wage : " + empWage);
+					}
+					System.out.println("Total Working Hrs :" + totalEmpHrs);
+					System.out.println("Total Employee Wage :" + empWage);
+					} 
 
-				
+			
 
 	private static int empCheck() {
 		// TODO Auto-generated method stub
