59c932fac95e5f435a355029284b57592ac87fe9
rishika7-hub <srishika970@gmail.com>
2020-10-14 13:46:17 +0530

    Welcome message

---
+++ src/EmpWage.java
@@ -0,0 +1,10 @@
+
+public class EmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Problem");
+
+	}
+
+}
