f05cb2f8801a1093200ab124d353c42ff322841e
rishika7-hub <srishika970@gmail.com>
2020-10-14 16:59:54 +0530

    Wages for a month

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -8,7 +8,9 @@ public class EmpWage {
 	private static final int PART_TIME_WAGE=WAGE_PER_HOUR * PART_DAY_HOUR;
 	public static final int IS_PART_TIME=2;
 	public static final int IS_FULL_TIME=1;
+	private static final int WORKING_DAYS_PER_MONTH=20;
 	
+
 
 	public static void main(String[] args) {
 				// TODO Auto-generated method stub
@@ -17,7 +19,6 @@ public class EmpWage {
 				int empHrs = 0;
 				int empWage=0;
 				int empCheck = (int) Math.floor(Math.random() * 10 ) % 3;
-				
 				switch (empCheck()) {
 				case IS_FULL_TIME: 
 					empHrs = 8;
@@ -27,17 +28,30 @@ public class EmpWage {
 					break;
 				default:
 					break;
+				} 
+				
+				//Wages For Month
+					int wages = 0;
+					for (int i = 1; i <= WORKING_DAYS_PER_MONTH; i++) {
+						System.out.println("Day #" + i);
+						if (isEmployeePresent()) {
+							System.out.println("Present");
+							wages += DAILY_WAGE;
+						} else {
+							System.out.println("Absent");
+						}
+					}
+					return;
 				}
 				
-				// Employee Attendance
 				
-				if(isEmployeePresent()) {
-					System.out.println("Employee is Present");
-				} else {
-					System.out.println("Employee is Absent");
-			        }
-	}
-
+				
+				
+				
+				
+		
+				
+			
 	private static int empCheck() {
 		// TODO Auto-generated method stub
 		return 0;
