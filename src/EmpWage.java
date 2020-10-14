d6e7b49f5fe75845af27ca762570bdd92320233a
rishika7-hub <srishika970@gmail.com>
2020-10-14 14:04:48 +0530

    Employee Attendance

--- src/EmpWage.java
+++ src/EmpWage.java
@@ -1,10 +1,20 @@
-
+import java.util.*;
 public class EmpWage {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Problem");
+		// Employee Attendance
+		
+		if(isEmployeePresent()) {
+			System.out.println("Employee is Present");
+		} else {
+			System.out.println("Employee is Absent");
+	        }
+	    }
 
+	public static boolean isEmployeePresent() {
+		// TODO Auto-generated method stub
+		return new Random().nextBoolean();
 	}
-
 }
